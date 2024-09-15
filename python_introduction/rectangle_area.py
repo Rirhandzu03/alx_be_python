9f2ce160c9794efb4267a8c8dec2bb4754419260
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-09-15 20:15:02 +0200

    Area of a rectangle calculation

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,15 @@
+#rectangle_area.py
+
+length =10 #length of the rectangle
+width = 5 #width of the rectangle
+
+#Calculate the area of a rectangle
+Area = length * width
+
+#Print calculated area
+print("The area of the rectangle is:", Area)
+
+
+
+
+
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,16 +1,16 @@
 
+#Define the variables
+principal = 1000 #in dollars
+rate = 0.05 #annual interest rate(5%)
+time = 3 # time in years
 
-#Define variables
-
-principal = 1000 # principal amount in dollars
-rate = 0.05 # annual interest rate(5%)
-time = 3 # Time in years
-
-
-#calculate the simple interest
+#Calculate Simple interest rate"
 interest = principal * rate * time
 
 print(f"The simple interest is: {interest}")
+
+
+
 
 
 
