a2b4602ee3b4d0d987506789d3ed69502e445942
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-09-29 00:44:32 +0200

    Shopping list manager

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,17 @@
-# Defining a function called perform_operation
+
+# defining a function called perform_operations
 
 def perform_operation(num1, num2, operation):
 
     if operation == 'add':
         return num1 + num2
-
+    
     elif operation == 'subtract':
         return num1 - num2
-
+    
     elif operation == 'multiply':
         return num1 * num2
-
+    
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Cannot divide by zero."
@@ -18,7 +19,10 @@ def perform_operation(num1, num2, operation):
             return num1 / num2
         
     else:
-        return "Invalid operation."
+        return "Invalid operation. "
+    
+    
+    
               
 
 
