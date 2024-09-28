35fa5e8a0653f2bcd70780435f03a36e083a0a7a
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-09-29 00:47:00 +0200

    Shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,7 +14,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter the item you want to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} has been added to the shopping list.")
 
