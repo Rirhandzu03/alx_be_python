a34f20bc1c932f82b7140f7cd7b1977c9c3e83bb
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-09-29 19:09:10 +0200

    Temperature conversion tool

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,10 @@
 from datetime import datetime, timedelta
 
-
 def display_current_datetime():
-
     current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-    print("Current date and time:", formatted_date)
-    
-    
+    print("current_date and time:", formatted_date)
+
 def calculate_future_date(days_in_future):
     current_date = datetime.now()
     future_date = current_date + timedelta(days=days_in_future)
@@ -15,17 +12,16 @@ def calculate_future_date(days_in_future):
 
     print(f"Future date after {days_in_future} days:", formatted_future_date)
 
-  # Main script
+
+#Main Script logic
 if __name__ == "__main__":
     display_current_datetime()
 
     try:
-        days = int(input("Enter the number of days to add to the current date: "))
+        days = int(input("Enter the numbers of days to add to current date: "))
         calculate_future_date(days)
     except ValueError:
-        print("Please enter a valid integer for the number of days.")
-
-
+        print("Please enter a valid integer for the number  of days.")
 
 
 
