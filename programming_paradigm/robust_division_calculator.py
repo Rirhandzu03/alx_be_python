5af98e894d237637e4cbfbae35405d97635029ba
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-10-06 18:31:46 +0200

    Division calculator with command line arguments

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,7 +7,7 @@ def safe_divide(numerator, denominator):
         result = num / den
         return f"The result of the division is {result}"
     except ZeroDivisionError:
-        return "Error: cannot divide by zero."
+        return "Error: Cannot divide by zero. "
     
     except ValueError:
         return "Error: please enter numeric values only."
